abc9ad5c5270d45a77861762fdae7c5cae56d405
precieux <precieuxnsiemo@hotmail.com>
2017-05-13 21:31:26 +0200

    Navbar first

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Princesse Location</title>
+
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="Ressources/CSS/home.css">
+<title>Princesse-Location</title>
+
 </head>
+
 <body>
-    Welcome !
+
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#images">Images</a></li>
+  <li><a href="#qr">Questions/Reponses</a></li>
+  <li><a href="#create">Créer une annonce</a></li>
+  <li><a href="#edit"></a>Modifier une annonce</li>
+  <li><a href="#others"></a>Autres annonces</li>
+  <li><a href="#contact"></a>Contact</li>
+  <li><a href="#identify"></a>S'identifier</li>
+</ul>
+
 </body>
 </html>
